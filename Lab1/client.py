d97ada591eaada3c643d13704d9233d4f608e7f3
fepeti13 <fepeti13@gmail.com>
2025-03-06 14:51:04 +0200

    lab1 explained version

--- Lab1/client.py
+++ Lab1/client.py
@@ -1,12 +1,12 @@
 #Ferencz Peter, 522/1
 
 from socket import *
-serverName = 'localhost'
+serverName = 'localhost'                            #megadjuk, hogy milyen cimre kuldje az infokat
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-sentence = input('Input lowercase sentence:')
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
+clientSocket = socket(AF_INET, SOCK_STREAM)         #megadja, hogy milyen IP-t es, hogy TCP-t fogunk hasznalni
+clientSocket.connect((serverName,serverPort))       #itt kapcsolodunk a szerverhez, letrejon a TCP kapcsolat
+sentence = input('Input lowercase sentence:')       #bekerjuk a szoveget    
+clientSocket.send(sentence.encode())                #elkuldjuk a servernek
+modifiedSentence = clientSocket.recv(1024)          #varjuk az infot a szervertol
 print ('From Server:', modifiedSentence.decode())
 clientSocket.close()
--- Lab1/server.py
+++ Lab1/server.py
@@ -1,18 +1,22 @@
 #Ferencz Peter, 522/1
 
 from socket import *
-serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-serverSocket.bind(('localhost',serverPort))
-serverSocket.listen(1)
+serverPort = 12000                                              #ezen a porton fog hallgatni a szerver
+serverSocket = socket(AF_INET,SOCK_STREAM)                      #letrehozunk egy uj TCP portot(SOCK_STREAM)
+                                                                #UDP eseten ez SOCK_DGRAM lenne
+                                                                #az AF_INET meghatarozza, hogy IPv4-et hasznaljunk
+                                                                #ami [0-255].[0-255].[0-255].[0-255] alakú
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)            #megengedi, hogy a portot ujrahasznaljuk, ezzel megelozve a "Address already in use" hibat
+serverSocket.bind(('localhost',serverPort))                     #letrehozza a szoketet, ami csak helyi kapcsolatokat fog fogadni
+                                                                #ezeket az 12000-es porton fogja hallgatni
+serverSocket.listen(1)                                          #megadja, hogy egyszerre csak 1 kliensre hallgasson
 print ('The server is ready to receive')
-while True:
-    connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    if sentence == "exit":
+while True:                                                     #addig varja a jeleket, ameddig mi kezzel le nem allitjuk(vagy exit kulcsoszoval a klienstol)
+    connectionSocket, addr = serverSocket.accept()              #egy uj szoketet hoz letre a klienssel valo kapcsolodashoz, az addr a kliens cime(IP, Port)
+    sentence = connectionSocket.recv(1024).decode()             #lekerjuk a kuldott uzenetet es dekodoljuk
+    if sentence == "exit":                                      #ha az uzenet exit volt, akkor kilepunk
         connectionSocket.send("I stopped".encode())
         exit()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
-    connectionSocket.close()
+    capitalizedSentence = sentence.upper()                      #nagybetusse alakitjuk
+    connectionSocket.send(capitalizedSentence.encode())         #es visszakuldjuk a kliensnek
+    connectionSocket.close()                                    #bezarjuk az adott socketet
