c1e5487fc202fac0c4b6d14db5902e3b461056b4
fepeti13 <fepeti13@gmail.com>
2025-03-06 09:22:52 +0200

    working provided code

--- Lab1/server.py
+++ Lab1/server.py
@@ -1,13 +1,14 @@
 from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind((‘’,serverPort))
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSocket.bind(('localhost',serverPort))
 serverSocket.listen(1)
-print ‘The server is ready to receive’
+print ('The server is ready to receive')
 while True:
-connectionSocket, addr = serverSocket.accept()
-sentence = connectionSocket.recv(1024).decode()
-capitalizedSentence = sentence.upper()
-connectionSocket.send(capitalizedSentence.
-encode())
-connectionSocket.close()
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence.
+    encode())
+    connectionSocket.close()
