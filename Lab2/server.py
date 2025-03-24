e1fb3920321524561fe98a04408138bb9a5e4edd
fepeti13 <fepeti13@gmail.com>
2025-03-25 00:47:04 +0200

    simple http connection

--- Lab2/server.py
+++ Lab2/server.py
@@ -1,5 +1,3 @@
-#Ferencz Peter, 522/1
-
 from socket import *
 serverPort = 12000                                              #ezen a porton fog hallgatni a szerver
 serverSocket = socket(AF_INET,SOCK_STREAM)                      #letrehozunk egy uj TCP portot(SOCK_STREAM)
@@ -9,15 +7,20 @@ serverSocket = socket(AF_INET,SOCK_STREAM)                      #letrehozunk egy
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)            #megengedi, hogy a portot ujrahasznaljuk, ezzel megelozve a "Address already in use" hibat
 serverSocket.bind(('localhost',serverPort))                     #letrehozza a szoketet, ami csak helyi kapcsolatokat fog fogadni
                                                                 #ezeket az 12000-es porton fogja hallgatni
-serverSocket.listen(1)                                          #megadja, hogy egyszerre csak 1 kliensre hallgasson
+serverSocket.listen(5)                                          #megadja, hogy egyszerre csak 1 kliensre hallgasson
 print ('The server is ready to receive')
 while True:                                                     #addig varja a jeleket, ameddig mi kezzel le nem allitjuk(vagy exit kulcsoszoval a klienstol)
     connectionSocket, addr = serverSocket.accept()              #egy uj szoketet hoz letre a klienssel valo kapcsolodashoz, az addr a kliens cime(IP, Port)
-    sentence = connectionSocket.recv(1024).decode()             #lekerjuk a kuldott uzenetet es dekodoljuk
-    if sentence == "exit":                                      #ha az uzenet exit volt, akkor kilepunk
-        connectionSocket.send("I stopped".encode())
-        connectionSocket.close()
-        exit()
-    capitalizedSentence = sentence.upper()                      #nagybetusse alakitjuk
-    connectionSocket.send(capitalizedSentence.encode())         #es visszakuldjuk a kliensnek
-    connectionSocket.close()                                    #bezarjuk az adott socketet
+    request = connectionSocket.recv(1024).decode()             #lekerjuk a kuldott uzenetet es dekodoljuk
+    
+    request_lines = request.split('\r\n')
+    for line in request_lines:
+        if line:
+            print(line)
+    #print(f"Received request: {request_line}")
+    
+    response_body = "<html><body><h1>Hello from my server!</h1></body></html>"
+    response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(response_body)}\r\nContent-Type: text/html\r\n\r\n{response_body}"
+
+    connectionSocket.send(response.encode())         #es visszakuldjuk a kliensnek
+    connectionSocket.close()     
